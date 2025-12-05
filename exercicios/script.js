ca84b4cf69d1d59e81e878888e5e57abacab84f6
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-05 16:55:56 -0300

    exercicio de variavel

---
+++ exercicios/script.js
@@ -0,0 +1,10 @@
+let letraA='A'
+let letraB='B'
+let letraC='C'
+
+console.log(letraA) 
+
+// não sei pq não funciona
+[letraA,letraB,letraC] = [letraB,letraC,letraA]
+
+console.log(letraA,letraB,letraC) 
