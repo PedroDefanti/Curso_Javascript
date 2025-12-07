b5978ca525f812c138e55072c50dfe6c2406f5dc
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-06 23:24:21 -0300

    aprendendo number

---
+++ material/number.js
@@ -0,0 +1,26 @@
+let num1 = 12
+let num2 = 2.5
+
+
+// transformar em  string 🔤
+
+// console.log(num1.toString()+num2)
+
+// num1=num1.toString()
+
+// binario
+
+console.log(num1.toString(2))
+
+
+// Casas decimais
+
+console.log(num1.toFixed(2))
+
+// Verificação de números inteiros
+
+console.log(Number.isInteger(num1))
+
+// verificação de número
+let temp=num1*'Olá'
+console.log(Number.isNaN(temp))
