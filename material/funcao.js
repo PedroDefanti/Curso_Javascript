a421e44ecf020f3a5f502962a8a69ad2bda599f7
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-06 23:23:52 -0300

    aprendendo função

---
+++ material/funcao.js
@@ -0,0 +1,21 @@
+// JEITO 1
+
+function raiz(n){
+    return n ** 0.5
+}
+
+const raiz_quadrada=raiz(9)
+console.log(raiz_quadrada)
+
+// JEITO 2
+
+let raiz_2=(n)=>{
+    return n ** 0.5
+}
+
+
+
+// eu posso diminuir o jeito 2
+
+const raiz_dois =n =>n **0.5
+console.log(raiz_dois(25))
