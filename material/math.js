5dd18f4c0adb65f947d55a275479313b2a49a8fa
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-06 23:24:07 -0300

    aprendendo math

---
+++ material/math.js
@@ -0,0 +1,25 @@
+let num1=4.9
+
+// Arredondamento
+
+// Pra -
+// let num2=Math.floor(num1)
+
+// Pra +
+// let num2=Math.ceil(num1)
+
+// Depende,se chegou/passou da metade,+ Pra,Se ñ pra -
+// let num2=Math.round(num1)
+
+// Saber o maior número
+// console.log(Math.max(1,2,3,4,5,-10,-50,150,6,7,8,9))
+
+// Saber o menor número
+// console.log(Math.min(1,2,3,4,5,-10,-50,150,6,7,8,9))
+
+// Número aleatorio
+// let aleatorio=Math.random()
+
+
+// O número pode Ser dividido por 0. Retornando Infinity
+
