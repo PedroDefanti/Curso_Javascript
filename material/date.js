b6773030ab8914eab44a7e2ec44190a73dc7d7d3
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-08 15:53:43 -0300

    apredendendo sobre objeto  date(imcompleto)

---
+++ material/date.js
@@ -0,0 +1,14 @@
+const data=new Date();
+//fala o dia/ mês/ ano/ hora/ minuto/ segundo
+
+// const data=new Date(); se eu colocar um 0 no Date ele retorna a darta de 1969(MUITO FODA)
+console.log(data.toString())
+
+// eu posso selecionar uma data especifica
+const data2=new Date(2019,3,20,15,14,32);
+
+console.log(data2.toString())
+
+// Eu posso pegar um valor específico
+
+const.
