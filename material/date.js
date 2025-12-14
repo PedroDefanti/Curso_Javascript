cdbe8f62862d3b05fcb71ddc5f4d652899333915
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-14 00:00:06 -0300

    Aprendendo Date

--- material/date.js
+++ material/date.js
@@ -11,4 +11,35 @@ console.log(data2.toString())
 
 // Eu posso pegar um valor específico
 
-const.
+const data_novo=new Date()
+console.log('Dia',data_novo.getDate())
+console.log('Mês',data_novo.getMonth()+1)// mês começa do zero
+console.log('Ano',data_novo.getFullYear())
+console.log('Hora',data_novo.getHours())
+console.log('Min',data_novo.getMinutes())
+console.log('Seg',data_novo.getSeconds())
+console.log('Ms',data_novo.getMilliseconds())
+console.log('Dia da semana',data_novo.getDay())//0 - Domingo, 6 - Sábado
+console.log(data_novo.toString())
+console.log(Date.now()) //retorna do ponto 0(1969) até a data atual
+
+function zeroAEsquerda(num){
+    return num>=10 ? num:`0${num}`
+}
+
+function formataData(data){
+    const dia=zeroAEsquerda((data.getDate()))
+    const mes=zeroAEsquerda((data.getMonth())+1)
+    const ano=zeroAEsquerda((data.getFullYear()))
+    const hora=zeroAEsquerda((data.getHours()))
+    const min=zeroAEsquerda((data.getMinutes()))
+    const seg=zeroAEsquerda((data.getSeconds()))
+
+    return `${dia}/${mes}/${ano} ${hora}: ${min}: ${seg}`
+
+}
+
+
+const data_a =new Date()
+const dataBrasil = formataData(data_a)
+console.log(dataBrasil)
