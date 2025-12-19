1b011eb376fca4eb05c47db6ca9911bdb6a52204
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-18 21:55:43 -0300

    metodos de objetos

---
+++ material/metodos_objs.js
@@ -0,0 +1,32 @@
+const produto={nome:'Caneca',preco:1.8}
+
+// mesmas coisas de jeitos diferentes
+
+// const outraCoisa={...produto,material:'Porcelana3'}
+
+// const outraCoisa=Object.assign({},produto,{material:'Porcelana'})
+
+//const outraCoisa={nome:produto.nome,preco:produto.preco}
+
+/**
+Object.keys()pega as chaves
+
+Object.freeze() congela  objeto
+
+Object.getOwnPropertyDescriptor() mostra as propriedades definidas 
+mostradas no define_property.js
+
+ 
+ */
+
+
+
+Object
+Object
+Object
+Object
+
+outraCoisa.nome='AAAAAAA'
+outraCoisa.preco=2.5
+console.log(produto)
+console.log(outraCoisa)
