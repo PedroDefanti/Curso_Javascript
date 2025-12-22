2241feb8cd055c71e94f01212712a028e7f747d0
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-22 00:09:51 -0300

    add Pessoa class implementation in mod.js

---
+++ material/node_estudo/aula1/mod.js
@@ -0,0 +1,16 @@
+// const nome='luiz'
+// const sobrenome='Souza'
+
+// module.exports.nome=nome
+
+// exports.nome=nome
+// exports.sobrenome=sobrenome
+// // console.log(exports)
+
+class Pessoa{
+    constructor(nome){
+        this.nome=nome
+    }
+}
+
+exports.Pessoa=Pessoa
