d0c4201a3e424e921ead12b83512aca22556ee34
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-22 00:13:43 -0300

    add app.js to implement file reading and data rendering

---
+++ material/node_estudo/aula5/app.js
@@ -0,0 +1,29 @@
+const path=require('path')
+const caminhoArquivo = path.resolve(__dirname,'teste.json')
+const escreve=require('./modules/escrever')
+const ler=require('./modules/ler')
+
+// const pessoas=[
+//     {nome:'João'},
+//     {nome:'Maria'},
+//     {nome:'Eduardo'},
+//     {nome:'Luiza'},
+// ]
+
+// const json=JSON.stringify(pessoas,'',2)
+
+// escreve(caminhoArquivo,json)
+
+async function leArquivo(caminho){
+    const dados=await ler(caminho)
+    rederizaDados(dados)
+}
+
+function rederizaDados(dados){
+    dados=JSON.parse(dados)// transforma JSON EM JS
+    dados.forEach(element => { console.log(element.nome)
+        
+    });
+}
+
+leArquivo(caminhoArquivo)
