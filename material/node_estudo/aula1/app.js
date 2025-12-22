926c700ad95c505d86ee8b1261813877f645b9e4
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-22 00:09:39 -0300

    add initial implementation of Pessoa class in app.js

---
+++ material/node_estudo/aula1/app.js
@@ -0,0 +1,7 @@
+const { Pessoa }=require('./mod')
+
+const p1 =new Pessoa('luiz')
+console.log(p1.nome)
+// nem sempre é necessário passar o caminho a tipo de 
+// vc instalar o axios na hora de passar no require não 
+// repcisa passar o caminho somente o nome do arquivo
