d228164e8a18c5310f1d08fd2175ee0aac429118
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-22 18:44:41 -0300

    refactor: update PaginaInicial to include rendering options and clean up comments

--- material/node_estudo/aula6/src/controllers/homeControllers.js
+++ material/node_estudo/aula6/src/controllers/homeControllers.js
@@ -1,7 +1,22 @@
 exports.PaginaInicial = (req, res) => {
-    res.render('index')// apos a requisicao retorna
+    // const mensagem=req.flash('info','flash funcionado')
+    // // console.log( req.session.usuario)<- vai ficar por sete dias
+    // console.log(req.flash('info'))
+    res.render('index', {
+        // titulo: 'Este será o <span style="color: red;">titulo</span> da pagina',
+        numeros: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    })// apos a requisicao retorna
+    return
+
+
+
 }
 
-exports.TrataPost =(req,res)=>{
-    res.send('Sua nova rota de post' + req.body.nome)
+exports.TrataPost = (req, res) => {
+    res.send(req.body)
 }
+// HomeModel.create({// retorna uma promessa
+//     titulo:'Titulo de testes',
+//     descricao:'Uma descrição de testes'
+// }).then(dados=>console.log(dados)).catch(e=>{console.log(e)})
+// isso faz o model aparecer no terminal
