be16883fdbfd53a3643ec59e8bcd27b175953dd8
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-22 00:15:10 -0300

    add routes.js to define application routes and controllers

---
+++ material/node_estudo/aula6/routes.js
@@ -0,0 +1,12 @@
+// acho que isso é tipo a urls.py do django 
+
+const express=require('express')
+const route=express.Router()
+const homeController=require('./src/controllers/homeControllers')
+const contatoController=require('./src/controllers/contatoControllers')
+route.get('/',homeController.PaginaInicial)
+route.get('/contato',contatoController.PaginaInicial)
+route.post('/',homeController.TrataPost)
+
+module.exports=route
+
