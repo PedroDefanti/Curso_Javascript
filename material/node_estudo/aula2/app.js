df9de2bc44a3c6a03ecb6c612cb4df54ed3c8789
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2025-12-22 00:09:56 -0300

    add initial implementation of app.js with path module usage

---
+++ material/node_estudo/aula2/app.js
@@ -0,0 +1,8 @@
+// const Cachorro=require('./Z/mod2')
+
+// module
+
+const path=require('path')// mostra a pasta atual: __dirname
+console.log(path.resolve(__dirname))// o amx de retrocesso é o c:\
+// tbm  na __dirname eu posso avançar e/ou pedir para 
+// entrar em pastas
